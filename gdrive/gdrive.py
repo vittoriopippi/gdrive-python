4c720cb5f9240ef5b478789fa990a3dec85c2cbf
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-12-16 06:31:33 +0000

    Adding tarfile member sanitization to extractall()

--- gdrive/gdrive.py
+++ gdrive/gdrive.py
@@ -91,7 +91,26 @@ class GDrive:
             file.write(r.content)
 
         with tarfile.open(filename) as file:
-            file.extractall(GDrive.folder_name)
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(file, GDrive.folder_name)
         return filename
 
     def about(self):
